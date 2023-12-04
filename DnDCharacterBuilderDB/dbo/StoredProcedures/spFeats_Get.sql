2d8b01c389e0b0a0a3411636c4a81e530bd3de7e
Xenvy <jasiekburza@gmail.com>
2023-12-04 21:50:00 +0100

    Add missing spFeats_Get SP, add option to remove feats
    Added missing stored procedure for getting a single feat
    from DB and added functionality to buttons designed to
    remove feat selections.

---
+++ DnDCharacterBuilderDB/dbo/StoredProcedures/spFeats_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spFeats_Get]
+	@Id int
+AS
+begin
+	select [Id], [Name], [Description], [Requirements], [ParentFeatName], [FeatSubselections], [CanTakeMultiple], [Tags]
+	from dbo.[Feats]
+	where Id = @Id;
+end
